307f0868ad4223b1dbdc8acc4c28eaceb6ceca93
Sam Hoggatt <schoggatt@ksu.edu>
2020-03-30 18:35:33 -0500

    asdasda

--- UniversityRegistry/Data/Person.cs
+++ UniversityRegistry/Data/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace UniversityRegistry.Data
@@ -7,8 +8,10 @@ namespace UniversityRegistry.Data
     /// <summary>
     /// A class representing a person associated with the university
     /// </summary>
-    public class Person
+    public class Person : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         /// <summary>
         /// The next ID to assign to a newly-created person
         /// </summary>
@@ -19,37 +22,124 @@ namespace UniversityRegistry.Data
         /// </summary>
         public uint ID { get; private set; }
 
+        private string firstName;
         /// <summary>
         /// The person's first name
         /// </summary>
-        public string FirstName { get; set; }
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (firstName == value) return;
+                firstName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FirstName"));
+            }
+        }
 
+        private string lastName;
         /// <summary>
         /// The person's last name
         /// </summary>
-        public string LastName { get; set; }
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (lastName == value) return;
+                lastName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("LastName"));
+            }
+        }
 
+        private DateTime dateOfBirth;
         /// <summary>
         /// The person's date of birth
         /// </summary>
-        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfBirth
+        {
+            get { return dateOfBirth; }
+            set
+            {
+                if (dateOfBirth == value) return;
+                dateOfBirth = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("DateOfBirth"));
+            }
+        }
 
+        private bool active;
         /// <summary>
         /// If this person is active in the university (currently a part of the university)
         /// </summary>
-        public bool Active { get; set; }
+        public bool Active
+        {
+            get { return active; }
+            set
+            {
+                if (active == value) return;
+                active = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Active"));
+            }
+        }
 
+        private Role role;
         /// <summary>
         /// The person's role
         /// </summary>
-        public Role Role { get; set; }
-        
+        public Role Role
+        {
+            get => role;
+            set
+            {
+                if (role == value) return;
+                role = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Role"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsUndergraduateStudent"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsGraduateStudent"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsFaculty"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsStaff"));
+            }
+        }
+
+        public bool IsFaculty
+        {
+            get { return Role == Role.Faculty; }
+            set { Role = Role.Faculty; }
+        }
+
+        public bool IsUndergraduateStudent
+        {
+            get { return Role == Role.UndergraduateStudent; }
+            set { Role = Role.UndergraduateStudent; }
+        }
+
+        public bool IsGraduateStudent
+        {
+            get { return Role == Role.GraduateStudent; }
+            set { Role = Role.GraduateStudent; }
+        }
+
+        public bool IsStaff
+        {
+            get { return Role == Role.Staff; }
+            set { Role = Role.Staff; }
+        }
+
         /// <summary>
         /// Creates a new user, assigning them an ID
         /// </summary>
         public Person()
         {
             ID = NextID++;
+        }
+
+        /// <summary>
+        /// Returns a string identifying the person
+        /// </summary>
+        /// <returns>A string consisting of last name, first name, and ID</returns>
+        public override string ToString()
+        {
+            return $"{LastName}, {FirstName} [{ID}]";
         }
     }
 }
---
+++ UniversityRegistry/UI/EnumToBooleanConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace UniversityRegistry.UI
+{
+    /// <summary>
+    /// A class for converting enum values to booleans
+    /// for use in data binding
+    /// </summary>
+    public class EnumToBooleanConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts an enum value into a boolean by comparing it to the supplied parameter.  
+        /// If the value and parameter are the same, the method returns true; otherwise false
+        /// </summary>
+        /// <param name="value">The enum value to convert</param>
+        /// <param name="targetType">The type to convert to (should be a boolean)</param>
+        /// <param name="param">The enum value to compare to</param>
+        /// <param name="culture">The culture info</param>
+        /// <returns>True if the enum matches the param, false otherwise</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.Equals(parameter);
+        }
+
+        /// <summary>
+        /// Converts a boolean into an enum value.  If the boolean is true, the parameter is returned,
+        /// otherwise DependencyProperty.UnsetValue is returned (which unsets the control)
+        /// </summary>
+        /// <param name="value">The boolean to convert</param>
+        /// <param name="targetType">The type to convert (should be an enum)</param>
+        /// <param name="param">The enumeration value to check</param>
+        /// <param name="culture">The culture info</param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value) return parameter;
+            else return DependencyProperty.UnsetValue;
+        }
+    }
+}
